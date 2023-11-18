9c0711600cc481b77551d2dca1be303f4079c077
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2023-11-18 13:02:27 -0800

    Changed some more things from intake to claw i guess

--- src/main/java/org/sert2521/bunnybots2023/subsystems/Claw.kt
+++ src/main/java/org/sert2521/bunnybots2023/subsystems/Claw.kt
@@ -3,9 +3,10 @@ package org.sert2521.bunnybots2023.subsystems
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.sert2521.bunnybots2023.ElectronicIDs
 
-object IntakeSubsystem : SubsystemBase() {
-    private val clawMotor = CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless)
+object Claw : SubsystemBase() {
+    private val clawMotor = CANSparkMax(ElectronicIDs.clawMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
 
     init {
         clawMotor.idleMode = CANSparkMax.IdleMode.kBrake
