2c3840e5d650003f4ad857504a68b5b82df01c29
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2023-11-18 12:34:13 -0800

    Changed variable and branch names from "intake" to "claw."

--- src/main/java/org/sert2521/bunnybots2023/subsystems/IntakeSubsystem.kt
+++ src/main/java/org/sert2521/bunnybots2023/subsystems/IntakeSubsystem.kt
@@ -2,22 +2,21 @@ package org.sert2521.bunnybots2023.subsystems
 
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
-import edu.wpi.first.wpilibj.RobotController
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 object IntakeSubsystem : SubsystemBase() {
-    private val intakeMotor = CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless)
+    private val clawMotor = CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless)
 
     init {
-        intakeMotor.idleMode = CANSparkMax.IdleMode.kBrake
-        intakeMotor.setSmartCurrentLimit(10, 45)
+        clawMotor.idleMode = CANSparkMax.IdleMode.kBrake
+        clawMotor.setSmartCurrentLimit(10, 45)
     }
 
     fun setMotor(speed: Double) {
-        intakeMotor.set(speed)
+        clawMotor.set(speed)
     }
 
     fun stop() {
-        intakeMotor.stopMotor()
+        clawMotor.stopMotor()
     }
 }
