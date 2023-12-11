7f7cd86cb47a891374f86a44f644871f367e7782
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2023-12-11 15:24:43 -0800

    Removed some unnesecary comments I made (ᵔᴥᵔ)

--- src/main/java/org/sert2521/bunnybots2023/subsystems/FlywheelSubsystem.kt
+++ src/main/java/org/sert2521/bunnybots2023/subsystems/FlywheelSubsystem.kt
@@ -10,8 +10,6 @@ import org.sert2521.bunnybots2023.TunedConstants
 object FlywheelSubsystem : SubsystemBase() {
     val flywheelMotor = CANSparkMax(ElectronicIDs.flywheelMotorID, CANSparkMaxLowLevel.MotorType.kBrushless)
 
-    //this needs to be in the command
-    //val feedForward = SimpleMotorFeedforward(TunedConstants.flywheelS, TunedConstants.flywheelV)
     init{
         flywheelMotor.setIdleMode(CANSparkMax.IdleMode.kCoast)
     }
